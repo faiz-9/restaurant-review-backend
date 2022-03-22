be05c38826e26ce20cbc2591a0505d302a6d55d2
faiz-09 <faizan.ahmed@bitcs.in>
2022-03-22 12:35:34 +0530

    failing

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,11 @@ async function bootstrap() {
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
+
+// PACKAGE.jSON
+/*
+  "start:dev": "nest start --watch",
+  "start:debug": "nest start --debug --watch",
+   "start:prod": "node dist/main",
+
+*/
